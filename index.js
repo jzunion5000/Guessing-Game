acb5e3e87239b13134c4ecb452ddc8e82d9a36db
Jeff Zhang <jeffzhang@Jeffs-MBP.fios-router.home>
2022-08-25 12:55:54 -0400

    update

---
+++ index.js
@@ -0,0 +1,21 @@
+// the .input needs to be connected to .guess reset and .guess submit
+const input = document.querySelector(".input");
+const guessReset = document.querySelector(".guess-reset")
+const guessSubmit = document.querySelector(".guess-submit")
+
+// reset needs to clear eveything 
+// guessReset.addEventListener("click", function(){
+//     if(Number(input))
+//     return;
+// })
+
+guessSubmit.addEventListener("click", function ({
+    for(let i = 1; i < 101) {
+        return();
+}
+
+})
+
+// submit needs to submit whatever is connected to input
+// submit also will connect to the 5 boxes that indicate the numbers so far and save them until 5 guesses are used
+// current guess needs to be saved so that the number can stick 
